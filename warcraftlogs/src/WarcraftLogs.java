4f10fb43116ea8b76bda0b2d48ad3e1101821c66
christian.schmidt <christian.schmidt@sportradar.com>
2016-09-26 15:23:12 +0200

    added libs

--- warcraftlogs/src/WarcraftLogs.java
+++ warcraftlogs/src/WarcraftLogs.java
@@ -6,21 +6,27 @@ import com.jarlenai.wcl.domain.reports.Report;
 
 public class WarcraftLogs {
 
-	ArrayList<Long> zones_to_parse = new ArrayList<Long>(10);
+	ArrayList<Long> zones_to_parse = new ArrayList<Long>();
 
 	public static void main(String[] args) {
 		WarcraftLogs wl = new WarcraftLogs();
+		wl.init();
 		wl.start();
+	}
+
+	private void init() {
+		zones_to_parse.add((long) 10);
+
 	}
 
 	private void start() {
 		WarcraftLogsApi wla = WarcraftLogsApi.get("c1badab4cfee15ababc006b67c31c4b1");
 
-		List<Report> user_reports = wla.getReportsForUser("Oscuramente", Long.valueOf("0"), Long.valueOf("0"));
+		List<Report> user_reports = wla.getReportsForUser("Oscuramente", (long) 0, System.currentTimeMillis());
 
 		for (Report user_report : user_reports) {
 			if (zones_to_parse.contains(user_report.getZone())) {
-
+				System.out.println(user_report.getTitle());
 			}
 		}
 
